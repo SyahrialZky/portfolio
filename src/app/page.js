06cba569ad21affff701b52bc32ddfd32e1b84e0
SyahrialZky <syahrialrizky04@gmail.com>
2023-11-26 22:08:16 +0700

    add section hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import Image from "next/image";
+import HeroSection from "./components/hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        Welcome to the world of{" "}
-        <span className="text-blue-500 font-bold">NFTs</span>.
-      </h1>
+    <main className="flex min-h-screen flex-col container mx-auto">
+      <HeroSection />
     </main>
   );
 }
